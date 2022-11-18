cb2a604d608d5f783debe84bf5a37d63a237958c
Matt Plunkett <marketlinkresearch@gmail.com>
2022-11-18 18:55:06 +0000

    created like function, created retweet function

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [
+  {
+    handle: `@TomFromMySpace`,
+    profilePic: `images/troll.jpg`,
+    likes: 27,
+    retweets: 10,
+    tweetText: `I miss the old internet`,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+  },
+  {
+    handle: `@Elon ✅`,
+    profilePic: `images/musk.png`,
+    likes: 6500,
+    retweets: 234,
+    tweetText: `I didn't mean to break twitter, anyone want to buy it off me?`,
+    replies: [
+      {
+        handle: `@TomCruise ✅`,
+        profilePic: `images/tcruise.png`,
+        tweetText: `Yes! Sign me up! 😎🛩`,
+      },
+      {
+        handle: `@ChuckNorris ✅`,
+        profilePic: `images/chucknorris.jpeg`,
+        tweetText: `I went last year😴`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+  },
+  {
+    handle: `@bebo`,
+    profilePic: `images/flower.png`,
+    likes: 10,
+    retweets: 3,
+    tweetText: `Remember us?`,
+    replies: [
+      {
+        handle: `@InternetMusuem`,
+        profilePic: `images/overflow.png`,
+        tweetText: `oh, only too well.`,
+      },
+      {
+        handle: `@YummyCoder64`,
+        profilePic: `images/love.png`,
+        tweetText: `You were great! `,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+  },
+];
